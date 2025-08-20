a5071cbc05cac4f6a7fc37ab200d3b9ef044fd6b
Digital Esposure Online Services <131973180+Deos2023@users.noreply.github.com>
2025-08-20 11:30:50 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client"
+import { Loader } from "@react-three/drei"
+import Overlay from "./Overlay/Overlay"
+import Scene from "./Scene"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <>
+    <Scene />
+    <Overlay />
+    <Loader />
+  </>
+)

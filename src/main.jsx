9a431971ddea01b8f472cae94dc03c391bd6a902
Digital Esposure Online Services <131973180+Deos2023@users.noreply.github.com>
2025-10-04 11:42:40 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,28 @@ import { Loader } from "@react-three/drei"
 import Overlay from "./Overlay/Overlay"
 import Scene from "./Scene"
 import ScrollToTop from "./component/ScrolltoTop"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import BlogSec from "./section/BlogSec"
+import ContactForm from "./section/ContactForm"
+import TeamSection from "./section/TeamSec"
 
 const root = createRoot(document.getElementById("root"))
 
 root.render(
   <>
-    <Scene />
-    <Overlay />
+    
+    
     <Loader />
-    <ScrollToTop/>
+    
+    <BrowserRouter>
+    <Scene />
+      <Routes> 
+        <Route path="/" element={<Overlay />}/>
+        <Route path="/blog" element={<BlogSec/>}/>
+        <Route path="/team" element={<TeamSection/>}/>
+        <Route path="/contact" element={<ContactForm/>}/>
+      </Routes>
+      <ScrollToTop/>
+    </BrowserRouter>
   </>
 )
